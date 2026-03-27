ffc08d6e6fe5f04e410c482fc8446b1ca264f20f
Ernest-HY-Tang <ernesthytang@gmail.com>
2026-03-27 23:21:05 +0800

    Day 3: Vectorised Titration Engine with NumPy.
    Used the following:
    - np.linspace to create arrays,
    - np.abs and np.log10 for vectorised calculations,
    - np.where for conditional logic.

--- 03_Titration_Master/engine.py
+++ 03_Titration_Master/engine.py
@@ -1,66 +1,27 @@
 import math
 import numpy as np
 
-#Day 1 - Calculating the pH of an acid and base solution
-def pH_calculator(v_acid, c_acid, v_base, c_base):
+#Day 2,3 - Vectorised Ttration Engine with NumPy
+def pH_calculator(v_acid, c_acid, c_base):
 
-    m_acid = (v_acid / 1000)* c_acid    #assuming density = 1.00 gcm^(-3)
-    m_base = (v_base / 1000) * c_base
-    v_total = v_acid + v_base   #creating v_total now to avoid repeating "v_acid + v_base"
+    eq_point = (v_acid) * c_acid / c_base   #in cm^3
 
-    if any(x < 0 for x in [v_acid, c_acid, v_base, c_base]):
-        raise ValueError("Volumes and concentrations should be non-negative.")     #rejecting impossible inputs with `raise ValueError`
+    v_base = np.linspace(0, eq_point *2, 1000)  #in cm^3
+    net_moles = v_acid / 1000 * c_acid - v_base / 1000 * c_base
+    acid_mask = net_moles > 0
+    base_mask = net_moles < 0
 
-    if v_total == 0:
-        return 7.00     #the pH of an empty beaker is simply "7.00"
-    
-    if m_acid > m_base:
-        m_acid_excess = m_acid - m_base
-        c_proton = m_acid_excess / (v_total / 1000)
-        return - math.log10(c_proton)   #condensed 2 lines into 1 by returning the calculation instead a new variable
+    v_total = v_acid + v_base   #in cm^3
+    conc_h = np.abs(net_moles / v_total * 1000)
+    with np.errstate(divide='ignore'):  #avoid calculation error at the equivalence point
+        pH_values = np.where(acid_mask, - np.log10 (conc_h), np.where(base_mask, 14 + np.log10 (conc_h), 7.00))
+    return v_base, pH_values
 
-    elif m_base > m_acid:
-        m_base_excess = m_base - m_acid
-        c_hydroxide = m_base_excess / (v_total / 1000)
-        return 14 + math.log10(c_hydroxide)
-    
-    else:
-        return 7.00
-
-get_v_acid = float(input("Volume of a Strong Monoprotic Acid (mL): "))
+get_v_acid = float(input("Volume of a Strong Monoprotic Acid (cm^3): "))
 get_c_acid = float(input("Concentration of a Strong Monoprotic Acid (dm^-3): "))
-get_v_base = float(input("Volume of a Strong Monoprotic Base (mL): "))
 get_c_base = float(input("Concentration of a Strong Monoprotic Base (dm^-3): "))
 
-pH = pH_calculator(get_v_acid, get_c_acid, get_v_base, get_c_base)
+output_base, output_pH = pH_calculator(get_v_acid, get_c_acid, get_c_base)
 
-print(f"The pH of the solution is: {pH:.2f}")
-
-# Work in progress... Learning Numpy for the first time!
-#eq_point = (initial_v_acid) * c_acid / c_base
-
-#v_base_before_eq = np.arrange(0, eq_point, 0.1)
-#v_total = initial_v_acid + v_base_before_eq
-#m_acid = c_acid * initial_v_acid - c_base * v_base_before_eq
-#return math.log10(m_acid / v_total)
-#for y in np.diter(acid):
-#    m_acid = (v_acid / 1000)* c_acid
-#    m_base = (v_base / 1000) * c_base
-#    v_total = y + v_acid
-#    m_acid_excess = m_acid - m_base
-#    c_proton = m_acid_excess / (v_total / 1000)
-#    return - math.log10(c_proton)
-
-#base = np.arrange(eq_point + 0.1, eq_point * 2, 0.1)
-#for z in np.diter(base):
-
-
-#v_base = np.array([])
-#    m_acid = (v_acid / 1000)* c_acid
-#    for y < m_acid / c_base in np.nidter(v_base):
-#        v_total = y + v_acid
-#        m_base = y *
-#        m_acid_excess = m_acid - m_base
-#        c_proton = m_acid_excess / (v_total / 1000)
-    
-#    return - math.log10(c_proton)
+print(f"Initial pH: {output_pH[0:5]}")
+print(f"Final pH: {output_pH[-5:]}")
